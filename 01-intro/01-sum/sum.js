8f2afa1b4047ca616fde71b3a99b464c2ee885c9
andrski <andrski2009@gmail.com>
2024-12-03 10:01:29 +0300

    add solution for 01-intro 01-sum

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,3 +1,10 @@
 export default function sum(a, b) {
-  /* ваш код */
+  const argsValidationArray = [...arguments].filter((item) => typeof item !== 'number');
+
+  if (argsValidationArray.length)
+    throw new TypeError(`Arguments should be number, bad arguments: ${argsValidationArray.join(',')}`);
+
+  return [...arguments].reduce((prev, i) => prev += i, 0);
 }
+
+console.log('RES', sum(1, 2));
